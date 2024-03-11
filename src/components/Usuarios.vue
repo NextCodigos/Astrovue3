26cf1dba7fd17873cfd4847d00d4a020c163759c
NextCodigos <nebulanemesis372@gmail.com>
2024-03-11 13:29:27 +0100

    Añado componentes vue3

---
+++ src/components/Usuarios.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>Agregar Usuarios</h2>
+        <form @submit.prevent="agregarUsuario">
+            <input v-model="nuevoUsuario" placeholder="Nombre de usuario">
+            <button>Añadir Usuario</button>
+        </form>
+
+        <h2>Lista de Usuarios</h2>
+        <button @click="toggleLista">{{ mostrarLista ? 'Ocultar' : 'Mostrar' }} Lista</button>
+
+        <ul v-if="mostrarLista">
+            <li v-for="(usuario, index) in usuarios" :key="index">{{ usuario }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nuevoUsuario: '',
+            usuarios: ['Usuario 1', 'Usuario 2', 'Usuario 3'],
+            mostrarLista: false
+        };
+    },
+    methods: {
+        agregarUsuario() {
+            if (this.nuevoUsuario.trim() !== '') {
+                this.usuarios.push(this.nuevoUsuario);
+                this.nuevoUsuario = ''; // Limpiamos el campo de entrada
+            }
+        },
+        toggleLista() {
+            this.mostrarLista = !this.mostrarLista;
+        }
+    }
+};
+</script>
---
+++ src/components/VerificadorPalindromo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2>Verificar Palíndromo</h2>
+        <input ref="inputField" v-model="inputText" :disabled="verificando" placeholder="Ingrese una cadena de texto">
+        <button @click="verificarPalindromo">Verificar</button>
+        <p v-if="mostrarResultado">{{ resultado }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inputText: '',
+            resultado: '',
+            verificando: false,
+            mostrarResultado: false
+        };
+    },
+    methods: {
+        verificarPalindromo() {
+            this.verificando = true;
+            const texto = this.inputText.toLowerCase().replace(/\s/g, '');
+            const invertido = texto.split('').reverse().join('');
+            this.resultado = invertido === texto ? 'Es un palíndromo' : 'No es un palíndromo';
+            this.mostrarResultado = true;
+
+            setTimeout(() => {
+                this.verificando = false;
+                this.inputText = '';
+                this.resultado = '';
+                this.mostrarResultado = false;
+                this.$refs.inputField.focus();
+            }, 3000);
+        }
+    }
+};
+</script>
+
+<style>
+input {
+    margin-right: 10px;
+}
+</style>
